1cdd79473574234dbc72fa26028822cd2acac271
Morten Andersen <mortentoo@users.noreply.github.com>
2016-11-29 15:49:20 +0100

    Update tracker-preview.py

--- tracker-preview.py
+++ tracker-preview.py
@@ -1,4 +1,6 @@
-for node in nuke.selectedNodes("Tracker4"):
+# Quickly disable the tracker preview for all tracker nodes to speed up rendering (significantly!)
+
+for node in nuke.allNodes("Tracker4"):
     print node.name() + ' preview disabled.'
     node["zoom_window_behaviour"].setValue(4)
     node["zoom_window_filter_behaviour"].setValue(2)
