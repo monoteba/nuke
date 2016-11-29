0208407a64c5c5abf9874c6235321f329247090e
Morten Andersen <mortentoo@users.noreply.github.com>
2016-11-29 17:06:10 +0100

    Update tracker-preview.py

--- Productivity/tracker-preview.py
+++ Productivity/tracker-preview.py
@@ -3,7 +3,7 @@
 print '\n\nDisabling preview on trackers:'
 
 for node in nuke.allNodes("Tracker4"):
-    print node.name() + ' preview disabled.'
+    print '- ' + node.name() + ' preview disabled.'
     node["zoom_window_behaviour"].setValue(4)
     node["zoom_window_filter_behaviour"].setValue(2)
     node["keyframe_display"].setValue(3)
