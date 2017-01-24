613c4f33819aa6d3e3fdb94c7cb8326b1cb2d7fa
Morten Andersen <mortentoo@users.noreply.github.com>
2017-01-24 18:32:51 +0100

    Update render-sequentially.py

--- Productivity/render-sequentially.py
+++ Productivity/render-sequentially.py
@@ -1,5 +1,12 @@
 # renders all selected Write nodes one by one, based in their first/last frame limits
-for node in nuke.selectedNodes('Write'):
+
+nodes = nuke.selectedNodes('Write')
+
+# sort by render order
+nodes.sort(key=lamba x: x['render_order'].value())
+
+# render!
+for node in nodes:
     print "\nRendering " + node['file'].value()
     if nuke.execute(node, int(node['first'].value()), int(node['last'].value())):
         print "CANCEL!"
