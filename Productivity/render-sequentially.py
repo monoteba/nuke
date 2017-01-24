b8422c069cbd77ebc35921285a65d1fc4c390e9a
Morten Andersen <mortentoo@users.noreply.github.com>
2017-01-24 18:25:44 +0100

    Update render-sequentially.py

--- Productivity/render-sequentially.py
+++ Productivity/render-sequentially.py
@@ -1,3 +1,7 @@
 # renders all selected Write nodes one by one, based in their first/last frame limits
 for node in nuke.selectedNodes('Write'):
-    nuke.execute(node, int(node['first'].value()), int(node['last'].value()))
+    print "\nRendering " + node['file'].value()
+    if nuke.execute(node, int(node['first'].value()), int(node['last'].value())):
+        print "CANCEL!"
+        break
+    print "...Done!"
