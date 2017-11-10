cf92adb32fa54d8d5e27db1856ad65aeb6af741e
Morten Andersen <mortenblaa@gmail.com>
2017-11-10 10:58:41 +0100

    single execute instead of executeMultiple

--- Productivity/render-node-by-node.py
+++ Productivity/render-node-by-node.py
@@ -18,11 +18,13 @@ def renderSelected():
     for i, node in enumerate(nodes):
         f = int(node['first'].value())
         l = int(node['last'].value())
+        nuke.execute(node, f, l, 1)
+        print("%d of %d, %s is done" % (i, c, node.name()))
         t = t + ((f, l, 1),)
 
     # execute multiple write nodes (writeNodes, ranges)
     # nuke.executeMultiple(tuple, tuple(tuple, tuple, tuple,))
-    nuke.executeMultiple(nodes, t)
+    #nuke.executeMultiple(nodes, t)
 
     # set proxy back to original value
     nuke.root()['proxy'].setValue(proxy)
