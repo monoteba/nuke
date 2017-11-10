0cdae59b845d1340a0f0c24f4cff1c1f72830cb5
Morten Andersen <mortenblaa@gmail.com>
2017-11-10 11:24:20 +0100

    fixes

--- Productivity/render-node-by-node.py
+++ Productivity/render-node-by-node.py
@@ -1,4 +1,4 @@
-# renders all selected Write nodes one by one, based in their first/last frame limits
+# renders all selected Write nodes one by one, sorted by render order and using first/last frame of the write node to determine the range to render.
 
 def renderSelected():
     nodes = nuke.selectedNodes('Write')
@@ -11,16 +11,19 @@ def renderSelected():
     nuke.root()['proxy'].setValue(False)
 
     # empty tuple for storing frame start/end/incr
-    t = ()
+    t = ()  # only used in executeMultiple()
 
     # render!
     c = len(nodes)
     for i, node in enumerate(nodes):
         f = int(node['first'].value())
         l = int(node['last'].value())
+        
+        # execute node
         nuke.execute(node, f, l, 1)
-        print("%d of %d, %s is done" % (i, c, node.name()))
-        t = t + ((f, l, 1),)
+        print("\n%d of %d, %s is done" % (i+1, c, node.name()))
+        
+        t = t + ((f, l, 1),)  # only used in executeMultiple()
 
     # execute multiple write nodes (writeNodes, ranges)
     # nuke.executeMultiple(tuple, tuple(tuple, tuple, tuple,))
@@ -30,4 +33,3 @@ def renderSelected():
     nuke.root()['proxy'].setValue(proxy)
 
 renderSelected()
-
