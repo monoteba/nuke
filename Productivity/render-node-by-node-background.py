84f8050ce46577abdf5bac0030ec380eb0b06a91
Morten Andersen <mortenblaa@gmail.com>
2017-11-13 08:27:18 +0100

    nuke background render, 2 jobs

--- Productivity/render-node-by-node-background.py
+++ Productivity/render-node-by-node-background.py
@@ -1,7 +1,4 @@
 # renders all selected Write nodes one by one, based in their first/last frame limits
-import time
-
-
 def renderSelected():
     nodes = nuke.selectedNodes('Write')
 
@@ -13,17 +10,27 @@ def renderSelected():
     nuke.root()['proxy'].setValue(False)
 
     # render!
+    ranges1 = nuke.FrameRanges()
+    ranges2 = nuke.FrameRanges()
+    nodes1 = []
+    nodes2 = []
     c = len(nodes)
-    print('\nRender %d nodes' % (c))
+
     for i, node in enumerate(nodes):
-        t = time.time()
         f = int(node['first'].value())
         l = int(node['last'].value())
-        nuke.execute(node, f, l)
-        lapse = time.time() - t
-        print('%d/%d: %s rendered in %.2f seconds' % (i+1, c, node.name(), lapse))
-
-
+        range = nuke.FrameRange('%d-%d' % (f,l))
+        if i % 2 == 0:
+            ranges1.add(range)
+            nodes1.append(node)
+        else:
+            ranges2.add(range)
+            nodes2.append(node)
+    
+    path = '/Applications/Nuke11.0v2/NukeX11.0v2 Non-commercial.app'
+    nuke.executeBackgroundNuke(path, nodes1, ranges1, ['main'], {})
+    nuke.executeBackgroundNuke(path, nodes2, ranges2, ['main'], {})
+    
     # set proxy back to original value
     nuke.root()['proxy'].setValue(proxy)
 
